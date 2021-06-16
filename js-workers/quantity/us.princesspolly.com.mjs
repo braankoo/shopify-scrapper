c9ee6b90629c2b04a773b1d48225236754cb9a5d
braankoo <lozinka007>
2021-06-16 14:12:14 +0200

    Update Phantom.JS

--- js-workers/quantity/us.princesspolly.com.mjs
+++ js-workers/quantity/us.princesspolly.com.mjs
@@ -23,26 +23,28 @@ function sliceIntoChunks(arr, chunkSize) {
 export default function (productId, csv) {
     fs.readFile(csv, 'utf8', function (err, data) {
         const variantsQuantity = data.match(/_BISConfig.product.variants\[\d]\['inventory_quantity'] = \d.*;/g);
-        variantsQuantity.forEach(function (row) {
-            const data = [...row.matchAll(/\d/g)];
-            const variant = data[0];
-            const quantity = data[1];
+        if (variantsQuantity.length > 0) {
+            variantsQuantity.forEach(function (row) {
+                const data = [...row.matchAll(/\d/g)];
+                const variant = data[0];
+                const quantity = data[1];
 
-            conn.query('SELECT variant_id FROM variants where product_id = ?', [productId], function (err, results) {
-                if (err) throw err;
-                const variantId = results[variant];
-
-                conn.query('UPDATE historicals SET inventory_quantity = ? WHERE variant_id = ? and date_created = CURDATE()', [quantity, variantId], function (err, results) {
+                conn.query('SELECT variant_id FROM variants where product_id = ?', [productId], function (err, results) {
                     if (err) throw err;
+                    const variantId = results[variant];
 
-                });
+                    conn.query('UPDATE historicals SET inventory_quantity = ? WHERE variant_id = ? and date_created = CURDATE()', [quantity, variantId], function (err, results) {
+                        if (err) throw err;
 
-                conn.query('SELECT sales FROM historicals WHERE variant_id = ? AND date_created = SUBDATE(CURDATE(),1)', [variantId], function (err, results) {
-                    if (err) throw err;
+                    });
 
-                });
-            })
-        });
+                    conn.query('SELECT sales FROM historicals WHERE variant_id = ? AND date_created = SUBDATE(CURDATE(),1)', [variantId], function (err, results) {
+                        if (err) throw err;
+
+                    });
+                })
+            });
+        }
 
 
     });

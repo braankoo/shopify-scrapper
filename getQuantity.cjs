44581cb3e7493116655e5c7a61c2d33626af3108
braankoo <lozinka007>
2021-06-21 11:49:17 +0200

    Add option to initialize fetching from frontend

--- getQuantity.cjs
+++ getQuantity.cjs
@@ -70,7 +70,7 @@ var pool = new Pool({
     workerTimeout: 300000
 });
 if (args.length > 0) {
-    conn.query("SELECT distinct CONCAT(REPLACE(product_json, '.json', ''), CONCAT('/', products.handle)) as url, products.product_id FROM sites INNER JOIN catalogs on sites.id = catalogs.site_id INNER JOIN catalog_product on catalogs.catalog_id = catalog_product.catalog_id INNER JOIN products on catalog_product.product_id = products.product_id  WHERE sites.id = ? AND products.position IS NOT NULL", [args[0]], (err, results, fields) => {
+    conn.query("SELECT distinct CONCAT(REPLACE(product_json, '.json', ''), CONCAT('/', products.handle)) as url, products.product_id FROM sites INNER JOIN catalogs on sites.id = catalogs.site_id INNER JOIN catalog_product on catalogs.catalog_id = catalog_product.catalog_id INNER JOIN products on catalog_product.product_id = products.product_id  WHERE sites.id = ? AND products.position IS NOT NULL and products.status = 'ENABLED'", [args[0]], (err, results, fields) => {
         if (err) throw err;
         console.log(results);
         results.forEach(function (result) {
@@ -81,7 +81,7 @@ if (args.length > 0) {
         pool.start();
     });
 } else {
-    conn.query("SELECT distinct CONCAT(REPLACE(product_json, '.json', ''), CONCAT('/', products.handle)) as url, products.product_id FROM sites INNER JOIN catalogs on sites.id = catalogs.site_id INNER JOIN catalog_product on catalogs.catalog_id = catalog_product.catalog_id INNER JOIN products on catalog_product.product_id = products.product_id WHERE products.position IS NOT NULL", (err, results, fields) => {
+    conn.query("SELECT distinct CONCAT(REPLACE(product_json, '.json', ''), CONCAT('/', products.handle)) as url, products.product_id FROM sites INNER JOIN catalogs on sites.id = catalogs.site_id INNER JOIN catalog_product on catalogs.catalog_id = catalog_product.catalog_id INNER JOIN products on catalog_product.product_id = products.product_id WHERE products.position IS NOT NULL and products.status = 'ENABLED'and products.status = 'ENABLED'", (err, results, fields) => {
         if (err) throw err;
         console.log(results);
         results.forEach(function (result) {

12e84e8863fff90ad51c9852d7dc421810b4983c
braankoo <lozinka007>
2021-06-22 11:31:15 +0200

    Add option to initialize fetching from frontend

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -50,7 +50,7 @@ class ProductController extends Controller {
             ->join('catalog_product', 'products.product_id', '=', 'catalog_product.product_id')
             ->join('catalogs', 'catalog_product.catalog_id', '=', 'catalogs.catalog_id')
             ->join('variants', 'products.product_id', '=', 'variants.product_id')
-            ->join('historicals', 'variants.variant_id', '=', 'variant_id')
+            ->join('historicals', 'variants.variant_id', '=', 'historicals.variant_id')
             ->when(!empty($filters->site->url), function ($q) use ($filters) {
                 $q->whereIn('sites.id', array_map(function ($site) {
                     return $site->id;

91943f2bcfd047b74b90143925fcadf2ce3cbadb
braankoo <lozinka007>
2021-06-23 17:52:56 +0200

    us.motelrocks.com.js get positions

---
+++ js-workers/phantom/position/us.motelrocks.com.js
@@ -0,0 +1,66 @@
+var webpage = require('webpage');
+var fs = require('fs');
+
+
+module.exports = function (data, done, worker) {
+    var params = data;
+
+    var page = webpage.create();
+    var pageId = 1;
+    var fail = 0;
+
+    const writeData = function (data) {
+        const path = 'data/position/' + params.hostname + '.csv';
+
+        fs.touch(path);
+        fs.write(path, data, 'a');
+    }
+    const loadPage = function (url, pageId) {
+
+        page.open(url + '&page=' + pageId, function (status) {
+
+            if (fail === 10) {
+                fail = 0;
+                loadPage(url, ++pageId);
+                return;
+            }
+            if (pageId === 11) {
+                done(null);
+            }
+
+
+            if (status !== 'success') {
+                fail++;
+
+                setTimeout(function () {
+                    loadPage(url, pageId);
+                }, 10000);
+                return;
+
+            }
+
+            const productsHtml = page.evaluate(function () {
+                return document.getElementById('bc-sf-filter-products').children.length;
+            });
+
+            if (productsHtml === 0) {
+                loadPage(url, pageId);
+
+
+            } else {
+                const content = page.evaluate(function () {
+                    return document.getElementById('bc-sf-filter-products').outerHTML;
+                });
+
+
+                const toWrite = content.match(/bc-product-json-.\d*/g);
+                writeData(toWrite.toString());
+                loadPage(url, ++pageId);
+
+            }
+
+        });
+
+    }
+    loadPage(data.url, 1);
+}
---
+++ js-workers/position/us.motelrocks.com.mjs
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const conn = await mysql.createConnection({
+    'host': process.env.DB_HOST,
+    'user': process.env.DB_USERNAME,
+    'password': process.env.DB_PASSWORD,
+    'database': process.env.DB_DATABASE,
+});
+
+
+export default function (csv) {
+    return new Promise(async function (resolve, reject) {
+        fs.readFile(csv, 'utf8', async function (err, data) {
+
+            let products = Array.from(new Set(data.match(/[0-9]{0,}/g)));
+
+            for (let i = 0; i < products.length; i++) {
+                await conn.query('UPDATE products SET position = ? WHERE product_id = ?', [i + 1, products[i]]);
+            }
+            resolve('true');
+        });
+
+
+    });
+
+}

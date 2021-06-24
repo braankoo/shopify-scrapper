5d03c861c159135d27e03f22fcac460033226616
braankoo <lozinka007>
2021-06-24 19:02:16 +0200

    ruct

---
+++ app/Jobs/GetPositionAndQuantity.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Site;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Str;
+use Symfony\Component\Process\Process;
+
+class GetPositionAndQuantity implements ShouldQueue {
+
+
+    public $timeout = 12000;
+    /**
+     * @var \App\Models\Site
+     */
+    public $site;
+
+
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Site $site)
+    {
+        $this->site = $site;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $process = new Process([ 'node', 'getPosition.cjs', 6 ], base_path());
+        $process->start();
+        $process->wait();
+        if (!Str::contains($this->site->product_json, [ 'tigermist', 'motelrocks' ]))
+        {
+            $process->wait();
+            $process = new Process([ 'node', base_path('getQuantity.cjs'), $this->site->id ], base_path());
+            $process->start();
+        }
+    }
+}

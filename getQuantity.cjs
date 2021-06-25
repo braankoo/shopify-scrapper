cede75c6b4ab7b69f7010bc0177065d7c46232f5
braankoo <lozinka007>
2021-06-25 10:00:13 +0200

    ruct

--- getQuantity.cjs
+++ getQuantity.cjs
@@ -62,10 +62,10 @@ function jobCallback(job, worker, index) {
 
 //
 var pool = new Pool({
-    numWorkers: 2,
+    numWorkers: 1,
     jobCallback: jobCallback,
     workerFile: __dirname + '/js-workers/quantity.js',
-    workerTimeout: 300000
+    workerTimeout: 1200000
 });
 if (args.length > 0) {
     conn.query("SELECT distinct CONCAT(REPLACE(product_json, '.json', ''), CONCAT('/', products.handle)) as url, products.product_id FROM sites INNER JOIN catalogs on sites.id = catalogs.site_id INNER JOIN catalog_product on catalogs.catalog_id = catalog_product.catalog_id INNER JOIN products on catalog_product.product_id = products.product_id  WHERE sites.id = ? AND products.position IS NOT NULL and products.status = 'ENABLED'", [args[0]], (err, results, fields) => {

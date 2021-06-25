f37ba4672d75bbbc6b42979192e6277cc7844b1e
braankoo <lozinka007>
2021-06-25 16:23:11 +0200

    ruct

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -131,7 +131,7 @@ class ProductController extends Controller {
         $filters = json_decode($request->input('filter'));
 
         return DB::table('products')
-            ->selectRaw('historicals.date_created, SUBSTRING_INDEX(sites.product_json,"/",3) as site,catalogs.title as catalog, products.title as product,image,CONCAT(CONCAT(CONCAT(SUBSTRING_INDEX(sites.product_json,"/",3), "/collections/"),catalogs.handle),CONCAT("/products/",products.handle)) as url, type,DATE_FORMAT(products.created_at, "%Y-%m-%d") as created_at,DATE_FORMAT(products.published_at, "%Y-%m-%d") as published_at, IFNULL(products.position,"n/a") as `products.position`,IFNULL(sum(sales),"n/a") as sales,quantity, products.id as product_id,ROUND((AVG(historicals.price)/1000000),2) as price')
+            ->selectRaw('historicals.date_created, SUBSTRING_INDEX(sites.product_json,"/",3) as site,catalogs.title as catalog, products.title as product,image,CONCAT(CONCAT(CONCAT(SUBSTRING_INDEX(sites.product_json,"/",3), "/collections/"),catalogs.handle),CONCAT("/products/",products.handle)) as url, type,DATE_FORMAT(products.created_at, "%Y-%m-%d") as created_at,DATE_FORMAT(products.published_at, "%Y-%m-%d") as published_at, IFNULL(products.position,"n/a") as `products.position`,IFNULL(sum(sales),"n/a") as sales,IFNULL(SUM(inventory_quantity),"n/a"), products.id as product_id,ROUND((AVG(historicals.price)/1000000),2) as price')
             ->join('variants', 'products.product_id', '=', 'variants.product_id')
             ->join('catalog_product', 'products.product_id', 'catalog_product.product_id')
             ->join('catalogs', 'catalog_product.catalog_id', 'catalogs.catalog_id')

4bbf9b18b7597486323d6954929b64b407ae584b
braankoo <lozinka007>
2021-06-29 20:40:32 +0200

    ruct

--- getPosition.cjs
+++ getPosition.cjs
@@ -50,7 +50,7 @@ function jobCallback(job, worker, index) {
 
                 } catch (err) {
 
-                    console.log(err);
+                    // throw err;
                 }
 
             });

bbde174dc0de307e281701dfeb5643106810a1ea
braankoo <lozinka007>
2021-07-02 21:48:20 +0200

    queue

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -66,6 +66,7 @@ class ProductController extends Controller {
             ->join('catalog_product', 'products.product_id', '=', 'catalog_product.product_id')
             ->join('catalogs', 'catalog_product.catalog_id', '=', 'catalogs.catalog_id')
             ->join('sites', 'catalogs.site_id', '=', 'sites.id')
+            ->join('sites', 'products.site_id', '=', 'sites.id')
             ->join('variants', 'products.product_id', '=', 'variants.product_id')
             ->leftjoin('historicals', 'variants.variant_id', '=', 'historicals.variant_id')
             ->when(!empty($filters->site->url), function ($q) use ($filters) {

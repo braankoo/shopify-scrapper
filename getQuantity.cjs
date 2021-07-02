c76a1c182631ff63d7342f6a12ace6afc5f8fc4e
braankoo <lozinka007>
2021-07-02 21:22:45 +0200

    queue

--- getQuantity.cjs
+++ getQuantity.cjs
@@ -57,7 +57,7 @@ function jobCallback(job, worker, index) {
 
                     });
                 } catch (err) {
-                    console.log(err);
+                    if (err) throw err;
                 }
             }
         );

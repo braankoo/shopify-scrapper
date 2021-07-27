98db74ac6b56fae7e3313361e7eb65926a9eeb65
braankoo <lozinka007>
2021-07-27 10:04:22 +0200

    queue

--- app/Jobs/GetPosition.php
+++ app/Jobs/GetPosition.php
@@ -39,8 +39,7 @@ class GetPosition implements ShouldQueue {
      */
     public function handle()
     {
-
-        $process = new Process([ 'pkill', '-f', "node getPosition.cjs $this->site->id" ], base_path());
+        $process = Process::fromShellCommandline("pkill -f node getPosition.cjs $this->site->id", base_path());
         $process->setTimeout(7000);
         $process->mustRun();
         $process->wait();
@@ -53,7 +52,7 @@ class GetPosition implements ShouldQueue {
 
     public function fail($exception = null)
     {
-        $process = new Process([ 'pkill', '-f', "node getPosition.cjs $this->site->id" ], base_path());
+        $process = Process::fromShellCommandline("pkill -f node getPosition.cjs $this->site->id", base_path());
         $process->setTimeout(7000);
         $process->mustRun();
         $process->wait();

dfb852298001dfc67fb111ab1a11d9a72f6a35b0
braankoo <lozinka007>
2021-08-16 16:52:16 +0200

    queue

--- app/Jobs/MergeData.php
+++ app/Jobs/MergeData.php
@@ -51,11 +51,11 @@ class MergeData implements ShouldQueue {
                             type,
                             DATE_FORMAT(products.created_at, '%Y-%m-%d')   as created_at,
                             DATE_FORMAT(products.published_at, '%Y-%m-%d') as published_at,
-                            products.position                              as `position`,
+                            product_position.position                              as `position`,
                             sum(sales)                                     as sales,
                             sum(quantity)                                  as quantity,
                             products.id                                    as product_id,
-                            date_created
+                            historicals.date_created
                     FROM historicals
                         INNER JOIN products
                     ON historicals.product_id = products.id AND products.site_id = historicals.site_id
@@ -63,6 +63,11 @@ class MergeData implements ShouldQueue {
                     ON products.id = catalog_product.product_id AND catalog_product.site_id = products.site_id
                         INNER JOIN catalogs on catalog_product.catalog_id = catalogs.id
                         INNER JOIN sites on products.site_id = sites.id
+
+                    inner join product_position
+                        on products.id = product_position.product_id
+                        and products.site_id = product_position.site_id
+                        and historicals.date_created = product_position.date_created
 
                     WHERE sites.id = :site_id
                     AND date_created = CURDATE()
